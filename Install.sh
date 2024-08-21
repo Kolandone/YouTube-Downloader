5871949e863ff62006c05f449cc93c7a9d0c5618
KOLAND <sjamshid81@gmail.com>
2024-08-21 19:26:15 +0330

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,13 +1,5 @@
 pkg update && pkg upgrade
 pkg install -y python
-curl -o $PREFIX/bin/koland https://raw.githubusercontent.com/Kolandone/workercreator/main/kol.py
-
-
+curl -o $PREFIX/bin/kolandyt https://raw.githubusercontent.com/Kolandone/Selector/main/Sel.sh
 chmod +x $PREFIX/bin/kolandyt
-
-
-sed -i '1i#!/usr/bin/env python' $PREFIX/bin/koland
-
-
-source $PREFIX/etc/profile
 echo "Installation complete. You can now run the script using the 'kolandyt' command."
