8dbf010c847ee0f479ae70590a80e31bb5c9b80b
KOLAND <sjamshid81@gmail.com>
2024-08-21 23:55:50 +0330

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,6 +2,6 @@ pkg update && pkg upgrade
 pkg install -y python
 pip install youtube-dl
 pkg install ffmpeg
-curl -o $PREFIX/bin/kolandyt https://raw.githubusercontent.com/Kolandone/YouTube-Downloader/main/Downloader.sh
+curl -o $PREFIX/bin/kolandyt https://raw.githubusercontent.com/Kolandone/YouTube-Downloader/main/Downloadr.sh
 chmod +x $PREFIX/bin/kolandyt
 echo "Installation complete. You can now run the script using the 'kolandyt' command."
